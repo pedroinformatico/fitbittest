ca7e234df5de12fee385df8cddc3f4f2e8b4dafb
Pedro Hernandez letelier <pedroinformatico@gmail.com>
2016-02-21 14:39:15 -0300

    nuevo

--- api/controllers/FitbitApiController.js
+++ api/controllers/FitbitApiController.js
@@ -11,9 +11,22 @@ module.exports = {
      */
     callBack: function (req, res) {
         var FitbitClient = require('fitbit-client-oauth2');
-        var clientId = 'YOUR_CLIENT_ID';
-        var clientSecret = 'YOUR_CLIENT_SECRET';
+        var clientId = '227G9R';
+        var clientSecret = '1160e5090c6ded5e6bd5c56a60863b4f';
         var client = new FitbitClient(clientId, clientSecret);
+
+        client.getToken(req.query.code, 'https://cl-pedrohernandez-fitbit-test.herokuapp.com/fitbit/callback')
+                .then(function (token) {
+                    // ... save your token on session or db ...
+                    FitbitToken.create(token).exec(function (err, token) {});
+                    // then redirect
+                    //res.redirect(302, '/user');
+                    res.send(token);
+                })
+                .catch(function (err) {
+                    // something went wrong.
+                    res.send(500, err);
+                });
 
 
     },
